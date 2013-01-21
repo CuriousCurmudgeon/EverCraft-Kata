3913814609be4c70e398e107fa19b9f7957dcdba
Brian Meeker <meeker.brian@gmail.com>
2013-01-20 22:06:10 -0500

    Iteration #1 - Tests for Character Can Be Damaged.

--- ruby/spec/character_spec.rb
+++ ruby/spec/character_spec.rb
@@ -55,9 +55,11 @@ describe Character do
 
   describe 'attack' do
     let(:armor_class) { 5 }
+    let(:hit_points) { 5 }
     let(:opponent) {
       c = Character.new
       c.armor_class = armor_class
+      c.hit_points = hit_points
       c
     }
 
@@ -66,12 +68,40 @@ describe Character do
     end
 
     it "hits if the die roll equals the opponent's armor class" do
-      subject.attack(opponent, armor_class).should be :hit
+      subject.attack(opponent, armor_class).should == :hit
+    end
+
+    it "is a critical hit on a natural 20" do
+      subject.attack(opponent, 20).should == :critical_hit
+    end
+
+    it "deals one point of damage on a hit" do
+      subject.attack(opponent, armor_class)
+      opponent.hit_points.should == hit_points - 1
+    end
+
+    it "deals two points of damage on a critical hit" do
+      subject.attack(opponent, 20)
+      opponent.hit_points.should == hit_points - 2
     end
 
     it "misses if the die roll is less than the opponent's armor class" do
       subject.attack(opponent, armor_class - 1).should be :miss
     end
+  end
+
+  describe "is dead?" do
+
+    it "returns true when their hit points are at 0" do
+      subject.hit_points = 0
+      subject.dead?.should be true
+    end
+
+    it "returns false if hit points are greater than 0" do
+      subject.hit_points = 1
+      subject.dead?.should be false
+    end
+
   end
 
 end
