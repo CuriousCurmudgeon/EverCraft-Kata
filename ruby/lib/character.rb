2faf9b1020f11cf898498918cc7753a5d2061306
Brian Meeker <meeker.brian@gmail.com>
2013-01-20 19:28:02 -0500

    Iteration #1 - Added implemenation for alignments.

--- ruby/lib/character.rb
+++ ruby/lib/character.rb
@@ -1,5 +1,14 @@
 class Character
 
+  ALIGNMENTS = [:good, :evil, :neutral]
+
   attr_accessor :name
+  attr_reader :alignment
+
+  # Only values in ALIGNMENT are valid
+  def alignment= value
+    raise unless ALIGNMENTS.any? {|a| a == value }
+    @alignment = value
+  end
 
 end
