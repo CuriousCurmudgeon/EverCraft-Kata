7598621433571a97dc22ebe6348d0eed8e51ad5f
Brian Meeker <meeker.brian@gmail.com>
2013-01-20 19:33:27 -0500

    Iteration #1 - Added implementation for hit points and armor_class.

--- ruby/lib/character.rb
+++ ruby/lib/character.rb
@@ -2,8 +2,13 @@ class Character
 
   ALIGNMENTS = [:good, :evil, :neutral]
 
-  attr_accessor :name
+  attr_accessor :name, :armor_class, :hit_points
   attr_reader :alignment
+
+  def initialize
+    @armor_class = 10
+    @hit_points = 5
+  end
 
   # Only values in ALIGNMENT are valid
   def alignment= value
