710f628e46aeef6f652864edc0df69ab805e6238
Brian Meeker <meeker.brian@gmail.com>
2013-01-20 19:27:43 -0500

    Iteration #1 - Added tests for alignments.

--- ruby/spec/character_spec.rb
+++ ruby/spec/character_spec.rb
@@ -2,7 +2,7 @@ require 'character'
 
 describe Character do
 
-  describe "Name" do
+  describe "name" do
     it "can be set" do
       subject.name = "Test"
       subject.name.should == "Test"
@@ -12,6 +12,23 @@ describe Character do
       subject.name = "Test"
       subject.name.should == "Test"
     end
+  end
+
+  describe "alignment" do
+
+    def test_valid_alignment alignment
+      subject.alignment = alignment
+      subject.alignment.should == alignment
+    end
+
+    it { test_valid_alignment(:good) }
+    it { test_valid_alignment(:evil) }
+    it { test_valid_alignment(:neutral) }
+
+    it "cannot be set to an invalid value" do
+      lambda { subject.alignment = :invalid }.should raise_error
+    end
+
   end
 
 end
