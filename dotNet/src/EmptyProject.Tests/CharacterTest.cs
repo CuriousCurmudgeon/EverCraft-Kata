95ee33232c472861c6e32eaa65ac95cc97625172
Brian Meeker <meeker.brian@gmail.com>
2013-02-08 14:55:56 -0500

    Iteration #1 - Implemented armor class and hit points accessors with defaults.

--- dotNet/src/EmptyProject.Tests/CharacterTest.cs
+++ dotNet/src/EmptyProject.Tests/CharacterTest.cs
@@ -17,6 +17,38 @@ namespace EverCraft.Tests {
 			var character = new Character { Alignment = Alignment.Evil };
 			character.Alignment.ShouldEqual(Alignment.Evil);
 		}
+
+		#region ArmorClass
+
+		[Test]
+		public void ArmorClass_defaults_to_10() {
+			var character = new Character();
+			character.ArmorClass.ShouldEqual(10);
+		}
+
+		[Test]
+		public void ArmorClass_can_be_initialized_to_a_different_value() {
+			var character = new Character(armor_class: 5);
+			character.ArmorClass.ShouldEqual(5);
+		}
+
+		#endregion
+
+		#region HitPoints
+
+		[Test]
+		public void HitPoints_defaults_to_5() {
+			var character = new Character();
+			character.HitPoints.ShouldEqual(5);
+		}
+
+		[Test]
+		public void HitPoints_can_be_initialized_to_a_different_value() {
+			var character = new Character(hit_points: 10);
+			character.HitPoints.ShouldEqual(10);
+		}
+
+		#endregion
 	}
 }
 
--- dotNet/src/EmptyProject/Character.cs
+++ dotNet/src/EmptyProject/Character.cs
@@ -10,11 +10,33 @@ namespace EverCraft {
 
 	public class Character {
 
-		public Character () {
+		public Character (int armor_class = 10, int hit_points = 5) {
+			this.ArmorClass = armor_class;
+			this.HitPoints = hit_points;
 		}
 
 		public string Name { get; set; }
+
+		/// <summary>
+		/// The alignment guides the characters actions.
+		/// </summary>
+		/// <value>
+		/// A value from the Alignment enum.
+		/// </value>
 		public Alignment Alignment { get; set; }
+
+		/// <summary>
+		/// Armor class is used to resist attacks from enemies.
+		/// </summary>
+		public int ArmorClass { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <value>
+		/// 
+		/// </value>
+		public int HitPoints { get; private set; }
 	}
 }
 
